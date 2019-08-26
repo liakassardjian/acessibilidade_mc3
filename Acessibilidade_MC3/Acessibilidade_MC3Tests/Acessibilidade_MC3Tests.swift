2099e759eb8ecc5abfe33875944bdf0b60f96f29
liakassardjian <liakassardjian@gmail.com>
2019-08-26 11:48:21 -0300

    Projeto do Xcode

---
+++ Acessibilidade_MC3/Acessibilidade_MC3Tests/Acessibilidade_MC3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Acessibilidade_MC3Tests.swift
+//  Acessibilidade_MC3Tests
+//
+//  Created by Lia Kassardjian on 26/08/19.
+//  Copyright © 2019 Lia Kassardjian. All rights reserved.
+//
+
+import XCTest
+@testable import Acessibilidade_MC3
+
+class Acessibilidade_MC3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
