e98871aef5b273ed0fe732e09a78dc42093b450c
Amaury A V A Souza <amauryavasouza@gmail.com>
2019-08-28 18:00:45 -0300

    Commit do primeiro dia de trabalho em avaliação

---
+++ Acessibilidade_MC3/Acessibilidade_MC3/Controller/AvaliarDeficienciasViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AvaliarDeficienciasViewController.swift
+//  Acessibilidade_MC3
+//
+//  Created by Amaury A V A Souza on 28/08/19.
+//  Copyright © 2019 Lia Kassardjian. All rights reserved.
+//
+
+import UIKit
+
+class AvaliarDeficienciasViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
