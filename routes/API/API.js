426adf55cb5afde47cec0be9924c7c066527db6e
Luiz Henrique Monteiro de Carvalho <lhmcarvx5@gmail.com>
2019-08-29 12:45:07 -0300

    Rotas do servidor atualizadas na API

--- routes/API/API.js
+++ routes/API/API.js
@@ -43,6 +43,7 @@ router.post('/login', async function (req, res, next) {
 
 
 //Empresas
+//cria uma empresa
 router.post('/createEmpresa', async (req, res) => {
     try {
         await Empresa.create(req.body)
@@ -53,10 +54,12 @@ router.post('/createEmpresa', async (req, res) => {
         res.json(false)
     }
 })
+//retorna todas as empresas
 router.get('/empresas', async (req, res) => {
     let empresas = await Empresa.find({})
     res.json(empresas)
 })
+//retorna uma empresa
 router.get('/empresaNome/:id', async (req, res) => {
     try {
         let empresa = await Empresa.findById(req.body._id)
@@ -67,36 +70,101 @@ router.get('/empresaNome/:id', async (req, res) => {
         res.json(false)
     }
 })
+//deleta uma empresa
 router.delete('/deleteEmpresa/:id', async (req, res) =>{
-    
+    try {
+        let idEmpresa = req.body._id
+        await Empresa.findByIdAndRemove(idEmpresa)
+        res.json(true)
+        
+    } catch (err) {
+        console.log('Error: ', err)
+        res.json(false) 
+    }
 })
 
 //Avaliacao
+//cria avaliacao
 router.post('/createAvaliacao', async (req, res) => {
-
+    try {
+        await Avaliacao.create(req.body)
+        res.json(true)
+    }
+    catch (err) {
+        console.log('Error: ', err)
+        res.json(false)
+    }
 })
+//retorna todas as avaliacoes
 router.get('/avaliacao', async (req, res) =>{
-
+    let avaliacoes = await Avaliacao.find({})
+    res.json(avaliacoes)
 })
+//faz update da avaliacao quando algo for alterado
 router.put('/updateAvaliacao/:id', async (req, res) => {
-
+    try {
+        await Avaliacao.findByIdAndUpdate(req.body)
+    } catch (err) {
+        console.log('Error: ', err)
+        res.json(false)
+    }
 })
+//deleta uma avaliacao
 router.delete('/deleteAvaliacao/:id', async (req, res) =>{
-
+    try {
+        let idAvaliacao = req.body._id
+        await Avaliacao.findByIdAndRemove(idAvaliacao)
+        res.json(true)
+        
+    } catch (err) {
+        console.log('Error: ', err)
+        res.json(false) 
+    }
 })
 
 //Usuario
+//cria um usuario
 router.post('/createUsuario', async (req, res) =>{
-
+    try {
+        await Usuario.create(req.body)
+        res.json(true)
+    }
+    catch (err) {
+        console.log('Error: ', err)
+        res.json(false)
+    }
 })
-router.get('/usuario/: id', async(req, res) =>{
-
+//retorna um usuario em especifico
+router.get('/usuario/:id', async(req, res) =>{
+    try {
+        let avaliacao = await Avaliacao.findById(req.body._id)
+        res.json(avaliacao)
+    }
+    catch (err){
+        console.log('Error ', err)
+        res.json(false)
+    }
 })
-router.put('/updateUsuario/: id', async(req, res) =>{
-
+//faz um update do usuario quando algo for mudado
+router.put('/updateUsuario/:id', async(req, res) =>{
+    try {
+        await Usuario.findByIdAndUpdate(req.body)
+    } catch (err) {
+        console.log('Error: ', err)
+        res.json(false)
+    }
 })
-router.delete('/deleteUsuario/: id', async (req, res)=>{
-    
+//deleta um usuario especifico
+router.delete('/deleteUsuario/:id', async (req, res)=>{
+    try {
+        let idUsuario = req.body._id
+        await Usuario.findByIdAndRemove(idUsuario)
+        res.json(true)
+        
+    } catch (err) {
+        console.log('Error: ', err)
+        res.json(false) 
+    }
 })
 
 
