c43618e1c56a39a33136563643fb53348e60893c
liakassardjian <liakassardjian@gmail.com>
2019-12-28 12:59:06 -0300

    Documentação da classe PickerController

--- Acessibilidade_MC3/Acessibilidade_MC3/Controller/PickerController.swift
+++ Acessibilidade_MC3/Acessibilidade_MC3/Controller/PickerController.swift
@@ -9,11 +9,33 @@
 import Foundation
 import UIKit
 
+/**
+ Classe que funciona como controlador para as Picker Views da interface.
+ 
+ A classe herda de NSObject, UIPickerViewDelegate e UIPickerViewDataSource, sendo, assim, Delegate e Data Source de uma determinada Picker View.
+ */
 class PickerController: NSObject, UIPickerViewDelegate, UIPickerViewDataSource {
     
+    /**
+     Lista de componentes a serem exibidos na Picker View.
+     
+     A lista de strings é inicializada quando um objeto desta classe é instanciado.
+     */
     var componentes: [String]
+    
+    /**
+     Item que é selecionado da Picker View.
+     
+     A string é inicializada como vazia quando um objeto desta classe é instanciado, mas recebe valor posteriormente.
+     */
     var selecionado: String
     
+    /**
+     Inicializador da classe.
+     
+     - parameters:
+        - componentes: Lista de strings que será exibida como opções da Picker View.
+     */
     init(componentes: [String]) {
         self.componentes = componentes
         self.selecionado = ""
