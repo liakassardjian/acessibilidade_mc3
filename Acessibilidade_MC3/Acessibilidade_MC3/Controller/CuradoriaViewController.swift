047d1ccba3834e1f2554ed5eb6bf4e27174a4a4b
Amaury A V A Souza <amauryavasouza@gmail.com>
2020-01-16 08:19:51 -0300

    setup inicial tela curadoria

---
+++ Acessibilidade_MC3/Acessibilidade_MC3/Controller/CuradoriaViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CuradoriaViewController.swift
+//  Acessibilidade_MC3
+//
+//  Created by Amaury A V A Souza on 16/01/20.
+//  Copyright © 2020 Lia Kassardjian. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class CuradoriaViewController: UIViewController {
+    
+    @IBOutlet weak var curadoriaTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
